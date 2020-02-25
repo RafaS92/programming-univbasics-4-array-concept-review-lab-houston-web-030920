9d60455714370844483e219a51f5ef054f3d9a79
Rafael Salvador Valdez <manrvaldez.92@gmail.com>
2020-02-25 03:20:22 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -22,7 +22,7 @@ def find_max_value(array)
     
 end
 
-def find_min_value(array))
+def find_min_value(array)
   l = array[0]
   array.length.times do |index |
     if array[index] < l
@@ -32,5 +32,4 @@ def find_min_value(array))
       
     l
     
-end
 end
