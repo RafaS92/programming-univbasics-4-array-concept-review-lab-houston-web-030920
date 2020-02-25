f071ecb86c4e8a40b8172f01a2e031e4df174811
Rafael Salvador Valdez <manrvaldez.92@gmail.com>
2020-02-25 02:57:23 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -3,8 +3,9 @@ def find_element_index(array, value_to_find)
   while count < array.length do
    if array[count] == value_to_find
     return array[count]
-    count += 1
+   
   end
+  count+=1
 end
 
 end
