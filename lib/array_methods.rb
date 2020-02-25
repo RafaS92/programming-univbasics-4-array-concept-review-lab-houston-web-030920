ecd44d4c024a01ee136b6f73aa5ff0cbf24e4444
Rafael Salvador Valdez <manrvaldez.92@gmail.com>
2020-02-25 02:29:00 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,6 +1,10 @@
 def find_element_index(array, value_to_find)
   count = 0 
-  while array.length < count
+  while array.length < count do
+    array[count] == value_to_find
+    return array[count]
+  end
+  
 end
 
 def find_max_value(array)
